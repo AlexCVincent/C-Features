2605d1498fce03abf1600f9fb7b381e4e7330a2a
AlexCVincent <alexandercvincent@gmail.com>
2017-10-12 12:05:04 +1100

    Did some of the steering behaviours work in class.

---
+++ Assets/9-SteeringBehaviours/Scripts/AIAgent.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI
+{
+    public class AIAgent : MonoBehaviour
+    {
+        public Vector3 force;
+        public Vector3 velocity;
+        public float maxVelocity = 100f;
+        public float maxDistance = 10f;
+        public bool freezeRotation = false;
+
+        private NavMeshAgent nav;
+        private List<SteeringBehaviour> behaviours;
+
+        void Start()
+        {
+            behaviours = new List<SteeringBehaviour>(GetComponents<SteeringBehaviour>());
+        }
+
+        void ComputeForces()
+        {
+            // SET force = Vector3 zero
+            force = Vector3.zero;
+            //FOR i = 0 to behaviours count
+            for (int i = 0; i < behaviours.Count; i++)
+            {
+                //LET behaviour = behaviours[i]
+                SteeringBehaviour behaviour = behaviours[i];
+                //IF behaviour is not active
+                if (behaviour.isActiveAndEnabled == false)
+                {
+                    //Continue
+                    continue;
+                }
+
+                //SET force = force + behaviour's force() * weighting
+                force = force + behaviour.force * behaviour.weighting;
+                //IF force's magnitude is greater than maxVelocity
+                if (force.magnitude > maxVelocity)
+                {
+                    //SET force = normalised force * maxVelocity
+                    force = force.normalized * maxVelocity;
+                    //break
+                    break;
+                }
+                }
+        }
+
+        void ApplyVelocity()
+        {
+            //SET velocity = velocity + force * deltatime
+            //IF velocity's magnitude > maxVelocity
+                //SET velocity = mormalised velocity * maxVelocity
+            // IF velocity's magnitude > 0
+                // SET transform.position = transform.position + velocity * delta time
+                //SET transform.rotation = Quaternion LookRotation (velocity)
+        }
+
+        void Update()
+        {
+            ComputeForces();
+            ApplyVelocity();
+        }
+    }
+
+}
---
+++ Assets/9-SteeringBehaviours/Scripts/SteeringBehaviours/Seek.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+    public class Seek : SteeringBehaviour
+    {
+        public Transform target;
+        public float stoppingDistance = 1f;
+        public override Vector3 GetForce()
+        {
+            //SET force to vector3 zero
+            force = Vector3.zero;
+            //IF target = null
+            if (target == null)
+            {
+                //return force
+                return force;
+            }
+
+            //LET desiredforce = target position - transform position
+            Vector3 desiredForce = target.position - transform.position;
+            //IF desiredForce magnitude  > stoppingDistance
+            if (desiredForce.magnitude > stoppingDistance)
+            {
+                //SET desiredForce = desiredForce normalised * weighting
+                desiredForce = desiredForce.normalized * weighting;
+                //SET force = desiredForce - owner.velocity
+                force = desiredForce - owner.velocity;
+            }
+            //Return force
+            return force;
+        }
+
+    }
+}
